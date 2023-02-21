aa8d3d7e34185fd2162f2f7a49d7cc3e351ea46c
tristan <tristan.greffe@hotmail.com>
2023-02-21 12:26:11 +0100

    build: setup server & mongoose

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT =  process.env.PORT || 5000
+
+mongoose.set('strictQuery', false);
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
