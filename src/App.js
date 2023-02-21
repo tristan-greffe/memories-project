d60f9cb9ce7d0a5a58a9a5db2e7003a194d85bef
tristan <tristan.greffe@hotmail.com>
2023-02-21 14:37:20 +0100

    feat: added app bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import Posts from './components/posts/Posts'
+import Form from './components/form/Form'
+
+import useStyles from './styles'
+
+import Memories from './assets/img/memories.png'
 
 const App = () => {
+  const classes = useStyles()
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <img className={classes.image} src={Memories} alt='memories' height='60'/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing={4}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
