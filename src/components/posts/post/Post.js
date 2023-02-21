91a8e0ed2a80f4f5efe6b4168e682755983166d5
tristan <tristan.greffe@hotmail.com>
2023-02-21 14:40:32 +0100

    initialized components

--- src/components/posts/post/Post.js
+++ src/components/posts/post/Post.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 import useStyles from './styles'
 
-const Form = () => {
+const Post = () => {
   const classes = useStyles()
   
   return (
-    <h1>FORM</h1>
+    <h1>POST</h1>
   )
 }
 
-export default Form
+export default Post
