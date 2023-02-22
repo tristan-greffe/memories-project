2b471209e3fdae2ac2267fbd2226f08f5ac868bd
tristan <tristan.greffe@hotmail.com>
2023-02-22 17:27:28 +0100

    feat: added code to like a post

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -6,6 +6,8 @@ const reducer = (posts = [], action) => {
       return action.payload
     case 'CREATE':
       return [...posts, action.payload]
+    case 'LIKE':
+      return posts.map((post) => (post._id === action.payload._id ? action.payload : post))
     case 'DELETE':
       return posts.filter((post) => post._id !== action.payload)
     case 'UPDATE':
